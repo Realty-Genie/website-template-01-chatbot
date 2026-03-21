6a8466daa4946faca17c527f909cb0213b88abb6
MohakGupta2004 <mohakgupta500@gmail.com>
2026-03-22 02:37:33 +0530

    script change

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,8 @@ const playfair = Playfair_Display({
 
 export const metadata: Metadata = {
   title: "Mai Real Estate | Exclusive Portfolio",
-  description: "Top 1% Realtor in Greater Vancouver. Powerful Luxury Marketing.",
+  description:
+    "Top 1% Realtor in Greater Vancouver. Powerful Luxury Marketing.",
 };
 
 export default function RootLayout({
@@ -35,11 +36,11 @@ export default function RootLayout({
         {children}
         <Whatsapp />
         <Chatbot />
-        
+
         <Script
           src="https://tracker-worker.green-feather-9c2c.workers.dev/tracker.js"
-          data-key="901e13d9-a617-4130-8c7f-ad2aa8809837"
-          strategy="afterInteractive" 
+          data-key="cb551051-cf98-40ee-9bdc-9a5d90544b65"
+          strategy="afterInteractive"
         />
       </body>
     </html>
